69a02ec3370395f8c13f48816da87ff6430db852
pradeep kumar patel <pradeepkumarpatel1998@gmail.com>
2025-06-15 01:04:48 +0530

    initial commit

---
+++ src/test/java/com/pradeep/students_server_registry/StudentsServerRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep.students_server_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsServerRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
